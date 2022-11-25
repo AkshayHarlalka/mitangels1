126d105c1a72df95f220eb379521bba6d8637832
AkshayHarlalka <akshayh1993@yahoo.com>
2022-11-24 18:55:24 -0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function init() {
-Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/179iJopQ1Zc2TmGZ5HVtkyiN4DQ4kzqEaFgeB08vx9_Q/pubhtml',
-callback: function(data, tabletop) {
-console.log(data)
-},
-simpleSheet: true } )
-}
-window.addEventListener(‘DOMContentLoaded’, init)
